87877bce21df2926860f088f9e56fff585f6342b
victor-vdo <37857818+victor-vdo@users.noreply.github.com>
2023-02-22 16:35:42 -0300

    Add project files.

---
+++ DesafioSeis/Cryptography.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioSeis
+{
+    public class Cryptography
+    {
+        public string Encrypt(string value)
+        {
+            var dic = new Dicionario();
+
+            var result = "";
+            foreach (var character in value)
+            {
+                result += dic.Caracteres.TryGetValue(character, out char val) ? val : Char.MinValue;
+            }
+            
+            return result;
+        }
+
+        public string Decrypt(string value)
+        {
+            var dic = new Dicionario();
+            var result = "";
+            foreach (var character in value)
+            {
+                result += dic.Caracteres.Where(c => c.Value == character).FirstOrDefault().Key;
+            }
+            return result;
+        }
+    }
+}
